f4826028f3e276bba27a84158e72d81a0a0ff010
ultrayoshi <david@imesmes.com>
2013-11-17 18:10:37 +0100

    Add concat and uglify grunt plugins

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,11 +15,29 @@ module.exports = function (grunt) {
                 files: ['js/**/*.js'],
                 tasks: ['jshint']
             }
+        },
+        concat: {
+            options: {
+                separator: ';',
+            },
+            dist: {
+                src: ['bower_components/jquery/jquery.js', 'js/main.js'],
+                dest: 'dist/built.js'
+            },
+        },
+        uglify: {
+            build: {
+                files: {
+                    'dist/built.js.min': 'dist/built.js'
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['jshint']);
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 };
