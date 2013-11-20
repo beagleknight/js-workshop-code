f560205e0a7da2f5fa1d56501e38bb40a76734cc
ultrayoshi <david@imesmes.com>
2013-11-20 19:55:38 +0100

    Fix exercise boss_stage_1 a bit

--- js/main.js
+++ js/main.js
@@ -65,27 +65,30 @@ $(function () {
     // 1. Create a function for creating character objects. They must have
     // id, name, strength, defense, hp and ap public attributes.
     
-    // 2. Create a function for creating player objects. They must inherit
-    // from characters and have attack and drinkPotion public methods
-    //
-    // attack method:
-    // receive an enemy parameter and substract from his hp the
-    // difference between player's strength and enemy defense. Also, ap is
-    // reduced by 1. 
-    // If ap is 0 it doesn't have any effect
-    // If all enemies are dead (hp <= 0) it doesn't have any effect
-    //
-    // drinkPotion method:
-    // reduce ap by 2 and increment hp by 1
-    // If ap is 0 it doesn't have any effect
-    // hp cannot be greater than initial hp
-    
-    // 3. Create a function for creating enemy objects. They must inherit
+    // 2. Create a function for creating enemy objects. They must inherit
     // from characters and have isDead public method
     //
     // isDead method:
     // returns true if hp <= 0
     
+    // 3. Create a function for creating player objects. They must inherit
+    // from characters and have attack and drinkPotion public methods
+    //
+    // attack method:
+    // receive an enemy parameter and substract from his hp the
+    // difference between player's strength and enemy defense. Also, 
+    // player's ap is reduced by 1. 
+    // If player's ap is 0 it doesn't have any effect
+    // If enemy's hp it doesn't have any effect
+    //
+    // drinkPotion method:
+    // reduce ap by 2 and increment hp by 1
+    // If ap is 0 it doesn't have any effect
+    
     // 4. Create 2 players and 3 enemies and add them to players and enemies
     // arrays. Use the two helpers above for DOM interaction using jquery.
+    // Example:
+    // bindActions(characters[0]);
+    // fillCharacterElement(characters[0]);
     // WARNING: character.id must be equal to the id of a DOM element
+});
