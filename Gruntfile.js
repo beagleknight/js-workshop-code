b93187f25632ac223fe3bc3c69287d1283f0fb74
ultrayoshi <david@imesmes.com>
2013-11-26 21:20:29 +0100

    Simplify start code for stage 6 exercises

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,56 +13,13 @@ module.exports = function (grunt) {
         watch: {
             scripts: {
                 files: ['js/**/*.js'],
-                tasks: ['jshint', 'concat', 'uglify']
-            },
-            styles: {
-                files: ['css/**/*.css'],
-                tasks: ['concat', 'cssmin']
-            }
-        },
-        concat: {
-            js: {
-                options: {
-                    separator: ';',
-                },
-                src: ['bower_components/jquery/jquery.js', 'js/utils.js', 'js/game.js', 'js/entity.js', 'js/player.js', 'js/crate.js', 'js/main.js'],
-                dest: 'dist/js/built.js'
-            },
-            css: {
-                src: ['css/reset.css', 'css/main.css'],
-                dest: 'dist/css/built.css'
-            }
-        },
-        uglify: {
-            build: {
-                files: {
-                    'dist/js/built.min.js': 'dist/js/built.js'
-                }
-            }
-        },
-        cssmin: {
-            combine: {
-                files: {
-                    'dist/css/built.min.css': 'dist/css/built.css'
-                }
-            }
-        },
-        csslint: {
-            options: {
-                csslintrc: '.csslintrc'
-            },
-            styles: {
-                src: ['css/**/*.css']
+                tasks: ['jshint']
             }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-csslint');
 
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify', 'cssmin']);
+    grunt.registerTask('default', ['jshint']);
 };
